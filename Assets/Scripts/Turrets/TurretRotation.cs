3d07783428477e0727f11e6efe603103acfbf678
Sitelinear7 <owen0234@gmail.com>
2024-03-25 16:12:12 -0400

    Changes to rotation

--- Assets/Scripts/Turrets/TurretRotation.cs
+++ Assets/Scripts/Turrets/TurretRotation.cs
@@ -7,6 +7,8 @@ public class TurretRotation : MonoBehaviour
     TurretSphereVision tSV;
     Quaternion originalRotation;
 
+    [SerializeField] Transform[] barrels;
+
     private void Start()
     {
         tSV = GetComponentInParent<TurretSphereVision>();
@@ -17,10 +19,17 @@ public class TurretRotation : MonoBehaviour
     {
         if(tSV.SpottedTargets.Count > 0)
         {
-            Vector3 direction = (tSV.SpottedTargets[0].position - transform.position).normalized;
-            Quaternion rotation = Quaternion.LookRotation(direction, Vector3.up);
+            Vector3 capDirection = new Vector3(tSV.SpottedTargets[0].position.x - transform.position.x, 0, tSV.SpottedTargets[0].position.z - transform.position.z).normalized;
+            Quaternion capRotation = Quaternion.LookRotation(capDirection, Vector3.up);
+            transform.rotation = capRotation;
 
-            transform.rotation = rotation;
+            Vector3 barrelDirection = (tSV.SpottedTargets[0].position - transform.position).normalized;
+            Quaternion barrelRotation = Quaternion.LookRotation(barrelDirection, Vector3.up);
+            for (int i = 0; i < barrels.Length; i++)
+            {
+                barrels[i].rotation = barrelRotation;
+            }
+
         }
         else
         {
